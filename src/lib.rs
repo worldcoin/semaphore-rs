3e7ff8061146b88fb8eaa95a3dd97d80ed356ea1
Remco Bloemen <remco@wicked.ventures>
2022-02-28 19:44:30 -0800

    Add benchmark

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@ mod test {
         tree.set(0, leaf.into());
 
         let merkle_proof = tree.proof(0).expect("proof should exist");
-        let root = tree.root().into();
+        let root = tree.root();
 
         // change signal and external_nullifier here
         let signal = "xxx".as_bytes();
@@ -52,7 +52,7 @@ mod test {
             generate_proof(&config, &id, &merkle_proof, external_nullifier, signal).unwrap();
         let success = verify_proof(
             &config,
-            &root,
+            &root.into(),
             &nullifier_hash,
             signal,
             external_nullifier,
@@ -61,5 +61,48 @@ mod test {
         .unwrap();
 
         assert!(success);
+    }
+}
+
+#[cfg(feature = "bench")]
+pub mod bench {
+    use crate::{
+        hash::Hash,
+        identity::Identity,
+        poseidon_tree::PoseidonTree,
+        protocol::{generate_nullifier_hash, generate_proof, verify_proof, SnarkFileConfig},
+    };
+    use criterion::Criterion;
+    use hex_literal::hex;
+
+    pub fn group(criterion: &mut Criterion) {
+        bench_proof(criterion);
+    }
+
+    fn bench_proof(criterion: &mut Criterion) {
+        // Create tree
+        let id = Identity::new(b"hello");
+        const LEAF: Hash = Hash::from_bytes_be(hex!(
+            "0000000000000000000000000000000000000000000000000000000000000000"
+        ));
+        let mut tree = PoseidonTree::new(21, LEAF);
+        let (_, leaf) = id.commitment().to_bytes_be();
+        tree.set(0, leaf.into());
+        let merkle_proof = tree.proof(0).expect("proof should exist");
+
+        // change signal and external_nullifier here
+        let signal = "xxx".as_bytes();
+        let external_nullifier = "appId".as_bytes();
+
+        let config = SnarkFileConfig {
+            zkey: "./snarkfiles/semaphore.zkey".to_string(),
+            wasm: "./snarkfiles/semaphore.wasm".to_string(),
+        };
+
+        criterion.bench_function("proof", move |b| {
+            b.iter(|| {
+                generate_proof(&config, &id, &merkle_proof, external_nullifier, signal).unwrap();
+            })
+        });
     }
 }
