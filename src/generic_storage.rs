c8aa974cc249fff73971f1452cf344eebec6bd33
Eric Woolsey <ewoolsey@ualberta.ca>
2024-04-15 14:39:41 -0700

    clean up unsafety

--- src/generic_storage.rs
+++ src/generic_storage.rs
@@ -8,12 +8,18 @@ pub use mmap_vec::MmapVec;
 pub trait GenericStorage<T>: Deref<Target = [T]> + DerefMut<Target = [T]> + Send + Sync {
     fn push(&mut self, value: T);
 
+    fn extend_from_slice(&mut self, slice: &[T]);
+
     fn clear(&mut self);
 }
 
-impl<T: Send + Sync> GenericStorage<T> for Vec<T> {
+impl<T: Send + Sync + Copy> GenericStorage<T> for Vec<T> {
     fn push(&mut self, value: T) {
         self.push(value);
+    }
+
+    fn extend_from_slice(&mut self, slice: &[T]) {
+        Vec::extend_from_slice(self, slice);
     }
 
     fn clear(&mut self) {
@@ -24,6 +30,10 @@ impl<T: Send + Sync> GenericStorage<T> for Vec<T> {
 impl<T: Send + Sync + Pod> GenericStorage<T> for MmapVec<T> {
     fn push(&mut self, value: T) {
         self.push(value);
+    }
+
+    fn extend_from_slice(&mut self, slice: &[T]) {
+        self.extend_from_slice(slice);
     }
 
     fn clear(&mut self) {
