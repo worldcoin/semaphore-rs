64b7dd69430e0aa423ac2c8037b7c6c6b632b1f0
Dzejkop <jakubtrad@gmail.com>
2024-09-24 15:38:24 +0200

    Bring lazy back + late trait bounds

--- crates/hasher/src/lib.rs
+++ crates/hasher/src/lib.rs
@@ -1,3 +1,5 @@
+use bytemuck::Pod;
+
 /// Hash types, values and algorithms for a Merkle tree
 pub trait Hasher {
     /// Type of the leaf and node hashes
@@ -6,3 +8,10 @@ pub trait Hasher {
     /// Compute the hash of an intermediate node
     fn hash_node(left: &Self::Hash, right: &Self::Hash) -> Self::Hash;
 }
+
+/// A marker trait that indicates some useful properties of a hash type
+///
+/// It's not strictly necessary, but for many implementations it's a useful set of constraints
+pub trait Hash: Pod + Eq + Send + Sync {}
+
+impl<T> Hash for T where T: Pod + Eq + Send + Sync {}
