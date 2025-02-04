22ddb4027e39f661bbae6a62881c360b5c7946ee
Dzejkop <jakubtrad@gmail.com>
2025-02-04 13:54:05 +0100

    fix

--- crates/js/src/lib.rs
+++ crates/js/src/lib.rs
@@ -62,9 +62,7 @@ fn from_array<const N: usize>(proof: js_sys::Array) -> Result<[U256; N], JsError
 fn to_array<const N: usize>(arr: [U256; N]) -> js_sys::Array {
     let ret = js_sys::Array::new();
     for v in arr {
-        let bytes: [u8; 32] = v.to_be_bytes();
-        let s = hex::encode(bytes);
-        let s = format!("0x{s}");
+        let s = format!("{v:#066x}");
         ret.push(&JsValue::from_str(&s));
     }
     ret
--- crates/semaphore/src/lib.rs
+++ crates/semaphore/src/lib.rs
@@ -8,15 +8,10 @@ pub mod packed_proof;
 pub mod poseidon_tree;
 pub mod protocol;
 
-use ark_bn254::Config;
-use ark_ec::bn::Bn;
 pub use semaphore_rs_depth_config::get_supported_depths;
 
 // Export types
 pub use crate::field::{hash_to_field, Field};
-
-pub type Groth16Proof = ark_groth16::Proof<Bn<Config>>;
-pub type EthereumGroth16Proof = ark_circom::ethereum::Proof;
 
 #[allow(dead_code)]
 #[cfg(test)]
