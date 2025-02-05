75251da644a726bc03c9980bb743455418c01a2d
Dzejkop <jakubtrad@gmail.com>
2025-02-05 10:50:32 +0100

    better TS typing

--- crates/js/src/lib.rs
+++ crates/js/src/lib.rs
@@ -3,67 +3,70 @@ use semaphore_rs_proof::{compression::CompressedProof, Proof};
 use wasm_bindgen::prelude::*;
 
 /// Compresses a Groth16 proof
-/// Input data must be an array of 8 hex encoded strings representing a valid proof
-/// 0x prefixes are optional
-///
-/// Returns an array of 8 hex encoded (0x prefixed) strings representing a compressed proof
-#[wasm_bindgen(js_name = "compressProof")]
-pub fn compress_proof(proof: js_sys::Array) -> Result<js_sys::Array, JsError> {
-    let proof = from_array(proof)?;
+#[wasm_bindgen(
+    js_name = "compressProof",
+    unchecked_return_type = "[string, string, string, string]",
+    return_description = "An array of 4 0x prefixed, hex encoded strings represeting a compressed proof"
+)]
+pub fn compress_proof(
+    #[wasm_bindgen(
+        unchecked_param_type = "[string, string, string, string, string, string, string, string]",
+        param_description = "An array of 8 hex encoded strings (with optional 0x prefixes) that represent an uncompressed proof"
+    )]
+    proof: Vec<String>,
+) -> Result<Vec<String>, JsError> {
+    let proof = from_vec(proof)?;
     let proof = Proof::from_flat(proof);
 
     let proof = semaphore_rs_proof::compression::compress_proof(proof)
         .ok_or_else(|| JsError::new("Failed to compress proof"))?;
     let proof = proof.flatten();
 
-    Ok(to_array(proof))
+    Ok(to_vec(proof))
 }
 
 /// Decompresses a Groth16 proof
-/// Input data must be an array of 4 hex encoded strings representing a valid compressed proof
-/// 0x prefixes are optional
-///
-/// Returns an array of 8 hex encoded (0x prefixed) strings
-#[wasm_bindgen(js_name = "decompressProof")]
-pub fn decompress_proof(proof: js_sys::Array) -> Result<js_sys::Array, JsError> {
-    let proof = from_array(proof)?;
+#[wasm_bindgen(
+    js_name = "decompressProof",
+    unchecked_return_type = "[string, string, string, string, string, string, string, string]",
+    return_description = "An array of 8 0x prefixed, hex encoded strings representing an uncompressed proof"
+)]
+pub fn decompress_proof(
+    #[wasm_bindgen(
+        js_name = "compressedProof",
+        unchecked_param_type = "[string, string, string, string]",
+        param_description = "An array of 4 hex encoded strings (with optional 0x prefixes) that represent a compressed proof"
+    )]
+    compressed_proof: Vec<String>,
+) -> Result<Vec<String>, JsError> {
+    let proof = from_vec(compressed_proof)?;
     let proof = CompressedProof::from_flat(proof);
 
     let proof = semaphore_rs_proof::compression::decompress_proof(proof)
         .ok_or_else(|| JsError::new("Failed to decompress proof"))?;
     let proof = proof.flatten();
 
-    Ok(to_array(proof))
+    Ok(to_vec(proof))
 }
 
-fn from_array<const N: usize>(proof: js_sys::Array) -> Result<[U256; N], JsError> {
-    if proof.length() as usize != N {
+fn from_vec<const N: usize>(proof: Vec<String>) -> Result<[U256; N], JsError> {
+    if proof.len() != N {
         return Err(JsError::new(&format!("Proof length must be {N}")));
     }
 
-    let mut proof_vals: Vec<U256> = Vec::with_capacity(N);
-    for i in 0..N {
-        let v = proof.get(i as u32);
-        let v = v
-            .as_string()
-            .ok_or_else(|| JsError::new("Proof items must be hex encoded strings"))?;
+    let proof: Vec<U256> = proof
+        .into_iter()
+        .map(|s| {
+            U256::from_str_radix(s.trim_start_matches("0x"), 16)
+                .map_err(|err| JsError::new(&err.to_string()))
+        })
+        .collect::<Result<_, _>>()?;
 
-        let v: U256 = U256::from_str_radix(v.trim_start_matches("0x"), 16)
-            .map_err(|err| JsError::new(&err.to_string()))?;
-
-        proof_vals.push(v);
-    }
-
-    let proof: [U256; N] = proof_vals.try_into().unwrap();
+    let proof: [U256; N] = proof.try_into().unwrap();
 
     Ok(proof)
 }
 
-fn to_array<const N: usize>(arr: [U256; N]) -> js_sys::Array {
-    let ret = js_sys::Array::new();
-    for v in arr {
-        let s = format!("{v:#066x}");
-        ret.push(&JsValue::from_str(&s));
-    }
-    ret
+fn to_vec<const N: usize>(arr: [U256; N]) -> Vec<String> {
+    arr.iter().map(|v| format!("{v:#066x}")).collect()
 }
