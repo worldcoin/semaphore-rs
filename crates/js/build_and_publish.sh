156c288b633ab7699feb7b8c8fc3077394baad73
Dzejkop <jakubtrad@gmail.com>
2025-02-10 16:28:36 +0100

    add missing js publish script change

--- crates/js/build_and_publish.sh
+++ crates/js/build_and_publish.sh
@@ -6,7 +6,7 @@ function build_and_publish_pkg() {
   target=$1
   
   wasm-pack build -t $target -d "pkg-$target"
-  jq --arg target "-$target" '.name += $target' "pkg-$target"/package.json > package.json.tmp && mv package.json.tmp "pkg-$target"/package.json
+  jq --arg target "-$target" '.name = "semaphore-rs" + $target' "pkg-$target"/package.json > package.json.tmp && mv package.json.tmp "pkg-$target"/package.json
 
   if [[ "${DRY_RUN,,}" == "true" || "$DRY_RUN" == "1" ]]; then
     echo "Skipping publish"
