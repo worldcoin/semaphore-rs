8fc0f77cc012a226be8a55aae95dffaa6b6adb99
pdtfh <149602456+pdtfh@users.noreply.github.com>
2025-05-07 09:25:06 -0600

    skip building circuit for docsrs

--- crates/semaphore/build.rs
+++ crates/semaphore/build.rs
@@ -89,8 +89,14 @@ fn build_circuit(depth: usize) -> Result<()> {
 }
 
 fn main() -> Result<()> {
+    // We don't build the circuit for `docsrs`, as the docsrs build doesn't have network access.
+    #[cfg(not(docsrs))]
     for depth in semaphore_rs_depth_config::get_supported_depths() {
         build_circuit(*depth)?;
     }
+
+    #[cfg(docsrs)]
+    println!("building for docsrs, skipping Semaphore circuit builds");
+
     Ok(())
 }
